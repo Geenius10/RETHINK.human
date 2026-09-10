7d5d4d392e14acd05cc5286d5909d0a9e50a00b5
Geenius10 <janngeorge9@gmail.com>
2026-09-10 16:18:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE="rethink-lingodeck-clean-v2-1";
-const ASSETS=["./","index.html","styles.css","app.js","manifest.webmanifest","icons/icon-192.svg","icons/icon-512.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match("index.html")))));
+const C="human-v6";const A=["./","index.html","manifest.webmanifest","logo.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
